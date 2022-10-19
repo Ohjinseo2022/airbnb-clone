46d1c4466b1b74db6b25c73cd0319a9e64c09415
Ohjinseo <dhwlstj@gmail.com>
2022-10-19 09:32:50 +0900

    데이터베이스 마이그레이션 복습

--- houses/models.py
+++ houses/models.py
@@ -2,10 +2,12 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
-"""Model Definition for Houses"""
+
 
 class House(models.Model):
+  """Model Definition for Houses"""
   name = models.CharField(max_length=140) #적당히 길이가 정해진 텍스트
-  price = models.PositiveIntegerField() # 정수and 양수 의 값
+  price_per_night = models.PositiveIntegerField() # 정수and 양수 의 값
   description = models.TextField() #길이가 정해지지않은 텍스트
   address = models.CharField(max_length=140)  #주소
+  pet_allowed = models.BooleanField(default=True) # 애완동물 동행가능 유무 
