1b65114416ca682cad8c912db5352df3ebf8e5ce
Ohjinseo <dhwlstj@gmail.com>
2022-10-21 11:03:27 +0900

    예약 리스트 DB 생성

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,55 @@
+from random import choices
 from django.db import models
+from common.models import CommonModel
 
 # Create your models here.
+class Booking(CommonModel):
+    """Bookings Model Definition"""
+
+    class BookingKindChoices(models.TextChoices):
+
+        ROOM = (
+            "room",
+            "Room",
+        )
+        EXPERIENCE = (
+            "experience",
+            "Ecperience",
+        )
+
+    kind = models.CharField(
+        max_length=15,
+        choices=BookingKindChoices.choices,
+    )
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experiences",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    check_in = models.DateField(
+        null=True,
+        blank=True,
+    )
+    check_out = models.DateField(
+        null=True,
+        blank=True,
+    )
+    experience_time = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    guests = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.kind.title()} Booking for : {self.user}"
